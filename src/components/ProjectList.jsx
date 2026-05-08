444c800749a4ac896c861b25aaf9e1653d2feabf
Sonia <sonia.wangari@student.moringaschool.com>
2026-05-08 11:41:07 +0300

    feat: complete SPA portfolio with CRUD, search, and responsive styling

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,14 +1,17 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
 function ProjectList({ projects }) {
   return (
     <div className="project-list">
-      {projects.map((project, index) => (
-        <div key={index} className="project-card">
-          <img src={project.image} alt={project.name} />
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-        </div>
+      {projects.map((project) => (
+        <ProjectCard 
+          key={project.id} // Better than index!
+          project={project} 
+        />
       ))}
     </div>
   );
 }
+
 export default ProjectList;
